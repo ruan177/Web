e63cfcf53bc69796a0b0c9260ad110c063048def
ruan177 <ruanpatrick177@gmail.com>
2023-05-13 10:39:00 -0300

    more changes

---
+++ src/components/pages/course.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react"
+import MarkdownPreview from "../MarkDownPreview"
+import { axios } from "../../lib/baseUrl";
+import { useParams } from "react-router-dom"
+import Header from "../headers/header";
+
+interface Course {
+    uuid: string,
+    name: string,
+    author: string,
+    body: string,
+}
+
+export function Course() {
+    const [postContent, setPostContent] = useState('');
+    const [course, setCourse] = useState<Course>();
+    const [error, setError] = useState('')
+    const { uuid } = useParams();
+
+    useEffect(() => {
+        const getCourse = async function () {
+            try {
+                const response = await axios.get(`/courses/${uuid}`,)
+                setCourse(response.data.course)
+                console.log(response.data.course)
+
+            } catch (error: any) {
+                setError(error.response.data.error)
+            }
+
+        }
+        getCourse();
+    })
+
+
+
+    return (
+
+        <div className=" flex justify-center items-center">
+            <div className="flex flex-col gap-8 ">
+                <Header />
+                
+                <div className="px-64 space-y-4">
+                    <h2 className="text-center ">{course?.name}</h2>
+                    <hr />
+                    <MarkdownPreview markdown={course?.body} />
+                </div>
+
+            </div>
+        </div>
+
+
+    );
+
+}
--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -1,98 +1,97 @@
-import axios from "axios";
+
 import { FormEvent, useContext, useState } from "react";
-import { NavLink ,useLocation, useNavigate } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { LoginContext } from "../../App";
-import { baseUrl } from "../../lib/baseUrl";
+import { axios } from "../../lib/baseUrl";
 import { Header } from "../headers/headerForm";
 
 
 
 
-export function Login (){
-    const {loggedIn, changeLoggedIn} = useContext(LoginContext)
+export function Login() {
+    const { loggedIn, changeLoggedIn } = useContext(LoginContext)
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError]= useState('')
+    const [error, setError] = useState('')
 
-    const handleSubmit = async function(event: FormEvent) {
+    const handleSubmit = async function (event: FormEvent) {
         event.preventDefault();
-        const url = baseUrl+"/login"
 
-        axios.post(url, {
-            data: ({
-                email, 
+
+        try {
+            const response = await axios.post("/login", {
+                email,
                 password
+            }, {
+                headers: {
+                    "Content-Type": "application/json"
+                }
             })
-        })
-        .then((response)=>{
-            if(response.status === 422){
-                throw new Error("Email or Password Incorrect")
-            }
-            
+
             localStorage.setItem('access', response.data.access)
             localStorage.setItem('refresh', response.data.refresh.id)
             localStorage.setItem('user', response.data.refresh.user_id)
-            
+
             changeLoggedIn(true)
             navigate('/')
-        })
-        .catch((error)=>{
-            setError(error.message)
-        }) 
+
+        } catch (error: any) {
+            setError(error.response.data.error)
+        }
     }
-   
-    return(
-    <div className="grid bg-gradient-to-r from-indigo-900 via-purple-500 to-rose-500">
-      <Header />
-    
-        <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-r from-indigo-900 via-purple-500 to-rose-500">
-        
-            <div className="w-full max-w-md flex flex-row gap-18 justify-center ">
-                <form onSubmit={handleSubmit} className="w-full shadow-md rounded px-8 pt-8 pb-10 mb-6 bg-white">
-                
-                <h1 className="mb-10 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Log-in</h1>
-                
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2">Email </label>
-                    <input 
-                        type="email" 
-                        id="email" 
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-                        placeholder="name@admin.com"
-                        onChange={event => setEmail(event.target.value)} 
-                        required></input>
+
+    return (
+        <div className="grid bg-gradient-to-r from-indigo-900 via-purple-500 to-rose-500">
+            <Header />
+
+            <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-r from-indigo-900 via-purple-500 to-rose-500">
+
+                <div className="w-full max-w-md flex flex-row gap-18 justify-center ">
+                    <form onSubmit={handleSubmit} className="w-full shadow-md rounded px-8 pt-8 pb-10 mb-6 bg-white">
+
+                        <h1 className="mb-10 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Log-in</h1>
+
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2">Email </label>
+                            <input
+                                type="email"
+                                id="email"
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                placeholder="name@admin.com"
+                                onChange={event => setEmail(event.target.value)}
+                                required></input>
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                                placeholder="**********"
+                                onChange={event => setPassword(event.target.value)}
+                                required></input>
+                            <p className="text-red-500 text-xs italic">{error}</p>
+                        </div>
+                        <div className="grid justify-items-center gap-6">
+                            <NavLink className="inline-block align-baseline font-bold text-sm text-violet-500 hover:text-violet-800" to="/register">
+                                Sign up
+                            </NavLink>
+                            <button className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-20 rounded-full focus:outline-none focus:shadow-outline" type="submit">
+                                Log in
+                            </button>
+
+                        </div>
+
+                    </form>
+
                 </div>
-                <div className="mb-6">
-                    <label  className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                        <input 
-                        type="password" 
-                        id="password" 
-                        className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
-                        placeholder="**********"
-                        onChange={event => setPassword(event.target.value)} 
-                        required></input>
-                    <p className="text-red-500 text-xs italic">{error}</p>
-                </div>
-                <div className="grid justify-items-center gap-6">
-                    <NavLink className="inline-block align-baseline font-bold text-sm text-violet-500 hover:text-violet-800" to="/register">
-                            Sign up
-                        </NavLink>
-                    <button className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-20 rounded-full focus:outline-none focus:shadow-outline" type="submit">
-                        Log in
-                    </button>
-                    
-                </div>
-                
-                </form>
-                
+
             </div>
-            
-        </div>
 
         </div>
 
-     
+
 
     )
 }
