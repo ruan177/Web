8289036e3d8c4287abd7cae8a715b97e65227f75
Ruan Patrick <114404700+ruan177@users.noreply.github.com>
2024-02-11 20:29:29 -0300

    change package json

--- src/pages/user/resetPassword.tsx
+++ src/pages/user/resetPassword.tsx
@@ -55,7 +55,10 @@ const ResetPasswordForm = () => {
                     disabled={isCodeSent || isResendButtonDisabled || isLoading} // Adiciona a condição isLoading
                   >
                     {isLoading ? (
-                      <span className="animate-spin">⚙️</span> // Substitua por um ícone de carregamento real se preferir
+                         <svg className="animate-spin h-5 w-5 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0  0  24  24">
+                         <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                         <path className="opacity-75" fill="currentColor" d="M4  12a8  8  0  018-8V0C5.373  0  0  5.373  0  12h4zm2  5.291A7.962  7.962  0  014  12H0c0  3.042  1.135  5.824  3  7.938l3-2.647z"></path>
+                     </svg>// Substitua por um ícone de carregamento real se preferir
                     ) : (
                       <FiMail />
                     )}
