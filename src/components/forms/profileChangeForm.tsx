4584749458c486a994896f43e4184666d5f44fe9
Ruan Patrick <114404700+ruan177@users.noreply.github.com>
2024-02-14 16:13:11 -0300

    error

--- src/components/forms/profileChangeForm.tsx
+++ src/components/forms/profileChangeForm.tsx
@@ -30,7 +30,7 @@ const ProfileChangeForm = () => {
             id="Username"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             value={username}
-            defaultValue={}
+            
             onChange={(e) => setUsername(e.target.value)}
           />
         </div>
