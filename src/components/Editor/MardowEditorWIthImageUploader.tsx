bf3c8317110ea80e64a6699aa59106ac49a9f074
Ruan Patrick <114404700+ruan177@users.noreply.github.com>
2024-10-08 09:42:29 -0300

    Update MardowEditorWIthImageUploader.tsx

--- src/components/Editor/MardowEditorWIthImageUploader.tsx
+++ src/components/Editor/MardowEditorWIthImageUploader.tsx
@@ -6,6 +6,7 @@ import MDEditor, {
   TextAreaTextApi,
 } from '@uiw/react-md-editor';
 import { RiImageAddLine } from 'react-icons/ri';
+
 import useAxios from '../../lib/axios';
 interface MarkdownEditorWithImageUploaderProps {
     bodyCourseContent: string;
@@ -18,51 +19,54 @@ function MarkdownEditorWithImageUploader({
   }: MarkdownEditorWithImageUploaderProps) {
   const axios = useAxios();
 
-  const addImageIcon = (
-    <RiImageAddLine size={20} />
-  );
+  const addImageIcon = <RiImageAddLine size={20} />;
 
-  const addImageCommand: ICommand = {
-    name: 'addImage',
-    keyCommand: 'addImage',
-    buttonProps: { 'aria-label': 'Add Image' },
-    icon: addImageIcon,
-    execute: async (state: TextState, api: TextAreaTextApi) => {
-      try {
-        // Abra um diálogo de seleção de arquivo para o usuário escolher a imagem
-        const fileInput = document.createElement('input');
-        fileInput.type = 'file';
-        fileInput.accept = 'image/*';
-        fileInput.style.display = 'none';
 
-        fileInput.addEventListener('change', async (event) => {
-          const target = event.target as HTMLInputElement;
-          if (target.files && target.files.length > 0) {
-            const file = target.files[0];
+
+  const customCommands: ICommand[] = [
+    {
+      name: 'addImage',
+      keyCommand: 'addImage',
+      buttonProps: { 'aria-label': 'Add Image' },
+      icon: addImageIcon,
+      execute: async (state: TextState, api: TextAreaTextApi) => {
+        try {
+          // Abra um diálogo de seleção de arquivo para o usuário escolher a imagem
+          const fileInput = document.createElement('input');
+          fileInput.type = 'file';
+          fileInput.accept = 'image/*';
+          fileInput.style.display = 'none';
+
+          fileInput.addEventListener('change', async (event) => {
+            const target = event.target as HTMLInputElement;
+            if (target.files && target.files.length > 0) {
+              const file = target.files[0];
          
-            if (file) {
-              // Enviar a imagem para o backend usando FormData
-              const formData = new FormData();
-              formData.append('file', file);
+              if (file) {
+                // Enviar a imagem para o backend usando FormData
+                const formData = new FormData();
+                formData.append('file', file);
          
-              // Realizar a solicitação POST para enviar a imagem para o servidor
-              const response = await axios.post('/upload', formData);
+                // Realizar a solicitação POST para enviar a imagem para o servidor
+                const response = await axios.post('/upload', formData);
          
-              // Verifique a resposta do servidor para obter a URL da imagem carregada
-              if (response.data.imageUrl) {
-                const imageMarkdown = `![Image](${response.data.imageUrl})`;
-                api.replaceSelection(imageMarkdown);
+                // Verifique a resposta do servidor para obter a URL da imagem carregada
+                if (response.data.imageUrl) {
+                  const imageMarkdown = `![Image](${response.data.imageUrl})`;
+                  api.replaceSelection(imageMarkdown);
+                }
               }
             }
-          }
          });
 
-        fileInput.click();
-      } catch (error) {
-        console.error('Erro ao fazer upload da imagem:', error);
-      }
+          fileInput.click();
+        } catch (error) {
+          console.error('Erro ao fazer upload da imagem:', error);
+        }
+      },
     },
-  };
+
+  ];
 
   return (
    
@@ -72,7 +76,8 @@ function MarkdownEditorWithImageUploader({
         value={bodyCourseContent}
         onChange={(value) => setBodyCourseContent(value || '')}
         commands={[
-          addImageCommand, // Adicione o novo comando aqui
+          ...commands.getCommands(),
+          ...customCommands,// Adicione o novo comando aqui
         ]}
       />
     
