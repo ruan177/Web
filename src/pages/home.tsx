f0895285fbc65acd1d63f1e1e882dcab87f4c79c
Ruan Patrick <114404700+ruan177@users.noreply.github.com>
2025-03-17 09:22:11 -0300

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,30 +7,28 @@ export function Home() {
     <>
       <Header />
       <div className="flex items-center justify-center h-screen bg-gray-100">
-       <div className="flex justify-between items-center  py-10 lg:py-0 border-y border-black xl:border-hidden xl:rounded-xl">
+        <div className="flex flex-col sm:flex-row justify-between items-center py-10 sm:py-6 lg:py-0 border-y border-black xl:border-hidden xl:rounded-xl">
           {/* Hero content */}
-          <div className="px-10 space-y-5 lg:py-6">
-            <h1 className="text-6xl md:text-7xl max-w-xl font-serif w-11/12 sm:w-9/12">
+          <div className="px-4 sm:px-10 space-y-5 lg:py-6 w-full sm:w-1/2 xl:w-1/2">
+            <h1 className="text-4xl sm:text-6xl md:text-7xl max-w-full sm:max-w-xl font-serif">
               <span className="underline decoration-black decoration-4">
                 MarkedLearn
               </span>{" "}
               is a place to mark, read, learn and connect.
             </h1>
-            <h2 className="w-9/12 font-normal">
+            <h2 className="font-normal">
               It's easy and free to post your thinking on any topic and connect with
               millions of readers.
             </h2>
-        
           </div>
           {/* Hero image */}
           <img
-            className="hidden sm:inline-flex h-40 lg:h-80 xl:h-full"
+            className="h-20 sm:h-40 lg:h-80 xl:h-full mx-auto sm:mx-0"
             src={logo}
             alt=""
           />
         </div>
-        </div>
-      
+      </div>
     </>
   );
 }
